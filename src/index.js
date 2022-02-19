4ca6f3b05ba5ce25b07456ab67e2ce15501a73a6
arialghifari <arialghifari99@gmail.com>
2022-02-19 15:36:39 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import "./main.css";
+
+const App = () => {
+    return <h1 className="text-3xl">Airbnb Experiences</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
