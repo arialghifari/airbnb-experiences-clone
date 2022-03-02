29d49f9481b5ac2bf7bf801b0f927e86c9b73589
arialghifari <arialghifari99@gmail.com>
2022-03-02 12:52:27 +0800

    Fix scroll-x bug

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,8 +10,8 @@ const Main = () => {
     return (
         <main className="my-10">
             <Hero />
-            <div className="flex justify-center mt-10 px-10 sm:pr-0">
-                <div className="cards flex gap-7 overflow-x-auto font-light text-sm pb-4 sm:pr-10">
+            <div className="flex justify-center mt-10">
+                <div className="cards flex gap-7 overflow-x-auto font-light text-sm pb-4 px-10">
                     {cardElements}
                 </div>
             </div>
